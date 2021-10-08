afb5ee0a299a6593fbf65d5caa5530e076ad199b
Matt Schlosser <hello@mattschlosser.me>
2021-10-08 17:34:19 -0600

    maybe

--- netlify/functions/whennext.js
+++ netlify/functions/whennext.js
@@ -1,5 +1,11 @@
 exports.handler = async function (event, context) {
     if (event.queryStringParameters) {
-        return event.queryStringParameters;
+        return {
+            statusCode: 200, 
+            headers: {
+                'Content-type': 'application/json'
+            }, 
+            body: JSON.stringify(event)
+        }
     }
 }
