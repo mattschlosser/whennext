35e1f17bb3d7a008cbe7fc691d5b9fced852702a
Matt Schlosser <hello@mattschlosser.me>
2021-10-08 17:32:24 -0600

    more netlify testing

--- netlify/functions/whennext.js
+++ netlify/functions/whennext.js
@@ -1,5 +1,5 @@
 exports.handler = async function (event, context) {
     if (event.queryStringParameters) {
-        console.log(event.queryStringParameters);
+        return event.queryStringParameters;
     }
 }
