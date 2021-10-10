abaaedaccd4165958677fcff375bd56bf59050be
Matt Schlosser <hello@mattschlosser.me>
2021-10-10 17:27:29 -0600

    fix cache maybe???

--- netlify/functions/whennext.js
+++ netlify/functions/whennext.js
@@ -66,7 +66,7 @@ exports.handler = async function (event, context) {
             let username = event.queryStringParameters.user;
             if (username) {
                 let user = await getUser(username)
-                headers['ETag'] = `"${Buffer.from(username, 'binary').toString('base64')}"`;
+                headers['Vary'] = `X-Cache-Bucket`;
                 let follows = await getFollows(user);
                 let upcomingStreams = [];
                 let promises = [];
